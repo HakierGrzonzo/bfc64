ce8e8001600bf02766b8736b831e72d89777cab0
Grzegorz Koperwas <grzes869@gmail.com>
2020-12-03 22:18:13 +0100

    asemmbly experiments

---
+++ processor/arch/c64.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+namespace assembly
+{
+    std::string inc =
+R"""(
+LDA $page,x
+)""";
+}
---
+++ processor/compiler.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include "symbols/symbol.h"
+#include <string>
+#include "arch/c64.hpp"
+
+
+std::string compiler(std::vector<Symbol> program)
+{
+
+}
